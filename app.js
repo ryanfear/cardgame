2726fa17c15815ddf847b91b9bc7bc75eec2514f
ryanfear <impulse.fx@gmail.com>
2018-07-02 14:27:20 -0600

    initial commit

---
+++ app.js
@@ -0,0 +1,46 @@
+
+
+let mainDeck = 
+[{name: "Bulbasaur", damage:60}, 
+{name: "Caterpie", damage:40},
+{name: "Charmander", damage:60},
+{name: "Clefairy", damage:50},
+{name: "Jigglypuff", damage:60},
+{name: "Mankey", damage:30},
+{name: "Meowth", damage:60},
+{name: "Nidoran - female", damage:60},
+{name: "Nidoran - male", damage:50},
+{name: "Oddish", damage:40},
+{name: "Pidgey", damage:50},
+{name: "Pikachu", damage:50},
+{name: "Poliwag", damage:50},
+{name: "Psyduck", damage:60},
+{name: "Rattata", damage:30}, 
+{name: "Squirtle", damage:60}, 
+{name: "Vulpix", damage:50}, 
+{name: "Weedle", damage:40}]
+
+let playerHand = [];
+let computerHand = [];
+let usedDeck = [];
+
+function newFunction() {
+    n = Math.floor(Math.random() * mainDeck.length);
+    return n;
+}
+
+function drawCards(mainDeck){
+    
+    for (let i = 0; i < 3; i++) {
+        n = Math.floor(Math.random() * mainDeck.length);
+        playerHand.push(mainDeck[n]);
+
+    }
+    return playerHand;
+};
+
+console.log(drawCards(mainDeck));
+
+
+
+
